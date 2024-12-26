aa3d57cc97de3e69e3b4324c86770356c397d6a6
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-12-26 02:40:17 +0200

    Add shoppingList component and fix eslint errors

--- app/index.tsx
+++ app/index.tsx
@@ -1,37 +1,12 @@
-import { Alert, Text, TouchableOpacity, View } from 'react-native'
+import { ShoppingListItem } from "@/components/ShoppingListItem";
+import { View } from "react-native";
 
 export default function Index() {
-  const handleDelete = () => {
-    Alert.alert(
-      'Are you sure you want to delete this?',
-      'It will be gone for good',
-      [
-        {
-          text: 'Yes',
-          onPress: () => console.log('Yes Pressed, Delete the item'),
-          style: 'destructive',
-        },
-        {
-          text: 'Cancel',
-          onPress: () => console.log('Cancel Pressed'),
-          style: 'cancel',
-        },
-      ]
-    )
-  }
   return (
-    <View className='flex-1 justify-center'>
-      <View className='flex-row items-center justify-between border-b-2 border-gray-200 border-b-sky-200 px-4 py-2'>
-        <Text className='text-3xl font-light'>Coffee</Text>
-        <TouchableOpacity
-          className='rounded-md p-4 bg-black'
-          onPress={handleDelete}
-        >
-          <Text className=' text-white font-bold uppercase tracking-wider'>
-            Delete
-          </Text>
-        </TouchableOpacity>
-      </View>
+    <View className="flex-1 justify-center gap-6">
+      <ShoppingListItem name="Coffee" />
+      <ShoppingListItem name="Tea" />
+      <ShoppingListItem name="Sugar" />
     </View>
-  )
+  );
 }
---
+++ components/ShoppingListItem.tsx
@@ -0,0 +1,35 @@
+import { View, Text, TouchableOpacity, Alert } from "react-native";
+
+export function ShoppingListItem({ name }: { name: string }) {
+  const handleDelete = () => {
+    Alert.alert(
+      `Are you sure you want to delete ${name} ?`,
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Yes Pressed, Delete the item"),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+      ],
+    );
+  };
+  return (
+    <View className="flex-row items-center justify-between border-b-2 border-gray-200 border-b-black px-4 py-2">
+      <Text className="text-3xl font-light">{name}</Text>
+      <TouchableOpacity
+        className="rounded-md p-4 bg-black"
+        onPress={handleDelete}
+      >
+        <Text className=" text-white font-bold uppercase tracking-wider">
+          Delete
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
